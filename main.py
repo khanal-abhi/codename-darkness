bb13d6c37a821d7918d32b2e5bd26841d210fe6d
Abhi Khanal <khanal.abhi@gmail.com>
2016-04-01 22:52:25 -0400

    Started.

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 #
 from google.appengine.ext import endpoints
+from protorpc import remote
+
+from models.message_models.task import Task
 from models.ndb_models import task
-from models.message_models import task, tasks
 
 
+@endpoints.api(name='tasks', version='v1', description='A simple task api')
+class TaskAPI(remote.service):
+    """This is the main class for the api. The methods defined here that are
+    decorated with the endpoints wrapper will be the endpoint methods."""
+    @endpoints.method(Task, Task,
+                      name='task',
+                      path='task',
+                      http_method='POST')
+    def insert_task(self, request):
+        new_task = task.Task(name=request.name, owner=request.owner).put()
+        return Task(name=new_task.name, owner=new_task.owner)
+
+
+Application = endpoints.api_server([TaskAPI], restricted=False)
